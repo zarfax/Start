813f2f0d417d546c9cd6d15f7c94c465a67d2b31
zarfax <espso@stud.ntnu.no>
2018-02-26 13:08:49 +0100

    Fixed sum

--- src/Solution.java
+++ src/Solution.java
@@ -5,13 +5,15 @@ class Solution
 {
     public static void main(String [] args) throws Exception
     {
+    	System.out.println("Write 2 numbers followed by an enter");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 1;
+        		//sc.nextInt();
         for(int t = 0; t < n; t++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int c = sc.nextInt();
-            System.out.println(a+b+c);
+            //int c = sc.nextInt();
+            System.out.println("The sum is "+(a+b));
         }
     }
 }
