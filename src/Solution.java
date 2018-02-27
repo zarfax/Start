9c8ea5b675de43a688f57a809e43a467291ca4be
zarfax <espso@stud.ntnu.no>
2018-02-27 13:37:15 +0100

    Changed a bit on the input prints

--- src/Solution.java
+++ src/Solution.java
@@ -5,12 +5,14 @@ class Solution
 {
     public static void main(String [] args) throws Exception
     {
-    	System.out.println("Write 2 numbers followed by an enter");
+    	
         Scanner sc = new Scanner(System.in);
         int n = 1;
         		//sc.nextInt();
         for(int t = 0; t < n; t++) {
+        	System.out.println("First Number");
             int a = sc.nextInt();
+            System.out.println("Second Number");
             int b = sc.nextInt();
             //int c = sc.nextInt();
             System.out.println("The sum is "+(a+b));
