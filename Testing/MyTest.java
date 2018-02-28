2df4d0cf7ac640274fe92efed859921d1c48112c
zarfax <espso@stud.ntnu.no>
2018-02-28 15:42:41 +0100

    Oppdaterer testing

--- Testing/MyTest.java
+++ Testing/MyTest.java
@@ -8,6 +8,7 @@ class MyTest {
 	public void test() {
 		TestMult tester =new TestMult();
 		 assertEquals(0, tester.mult(10, 0), "10 x 0 must be 0");
+		 assertEquals(2, tester.add(1, 1), "1 x 1 must be 1");
 		//fail("Not yet implemented");
 	}
 
--- src/TestMult.java
+++ src/TestMult.java
@@ -3,4 +3,8 @@ public class TestMult {
 	public int mult(int tall1, int tall2) {
 		return tall1 * tall2;
 	}
+
+	public int add(int tall1, int tall2) {
+		return tall1 + tall2;
+	}
 }
